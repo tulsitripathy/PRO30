9a952c4797fa85d0a3d9a6352bfcf026c9ddc64c
tulsitripathy <80155544+tulsitripathy@users.noreply.github.com>
2021-06-07 17:29:57 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,31 @@
+class Box{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          restitution : 0.7,
+          friction : 0.6,
+          isStatic : false,
+        }
+        this.visibility = 255;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        if (this.body.speed < 3) {
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }  else {
+        push();
+        World.remove(world, this.body);
+        this.visibility = this.visibility - 5;
+        tint(255, this.visibility);
+        pop();
+      }
+    }
+}
